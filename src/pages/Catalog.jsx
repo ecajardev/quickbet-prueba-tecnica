790922dcb56ca886442b74ee99c5dbe1fb7a855a
ecajardev <emanuelcajar.dev@gmail.com>
2025-01-17 13:44:46 -0500

    PageHeader fixed

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,6 +1,6 @@
 import { useParams } from 'react-router';
 
-import PageHeader from '../components/pageHeader/PageHeader';
+import PageHeader from '../components/PageHeader/PageHeader';
 
 import { category as cate } from '../api/tmdbApi';
 import MovieGrid from '../components/MovieGrid/MovieGrid';
